e06f18437feb6467ac5fa20c4b65a7d0de69bf1a
HH97108 <62311089+HH97108@users.noreply.github.com>
2021-04-19 08:16:42 +0800

    Update sqxs.js

--- Task/sqxs.js
+++ Task/sqxs.js
@@ -111,11 +111,11 @@ async function all()
 				drawgold=0;
 							
 				//阅读
-				//await readbook();	
+				await readbook();	
 				
 				//收集阅读金币
-				//if(ReadTimes>0)
-				//await receivecoin();
+				if(ReadTimes>0)
+				await receivecoin();
 				
 				//看视频奖励金币
 				await vediogoldprize(0);
