003683b226d3a2eb3425d14f5504ec7ada839d4e
HH97108 <62311089+HH97108@users.noreply.github.com>
2021-05-14 23:35:15 +0800

    Update youth2.js

--- Task/youth2.js
+++ Task/youth2.js
@@ -10,7 +10,7 @@
 const $ = new Env("中青看点")
 $.date = new Date();
 $.utc8 = $.date.getTime() + ($.date.getTimezoneOffset() * 60 * 1000) + 8 * 60 * 60 * 1000;
-let notifyInterval = $.getdata("notifytimes") || '100'; //通知间隔，默认抽奖每50次通知一次，如需关闭全部通知请设为0
+let notifyInterval = $.getdata("notifytimes") || 100; //通知间隔，默认抽奖每50次通知一次，如需关闭全部通知请设为0
 let ONCard = $.getdata('zqcard') || 'true'; //早起打卡开关
 let s = $.getdata('delay_rotary_zq') || '25'; //转盘延迟时间
 let withdrawcash = $.getdata('zqcash') || '30'; //提现金额
